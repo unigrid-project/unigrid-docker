3a7b791c59bbed8379c7813fdc7da775bccbe631
dekm <gevan73@gmail.com>
2022-10-17 18:14:06 +0200

    moving all docker setup scripts into the same project

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
 # Build command
-# docker build --no-cache . --tag=unigrid/unigrid-docker:beta
+# docker build --no-cache . --tag=unigrid/unigrid:beta
 # main release is latest
-# docker build --no-cache . --tag=unigrid/unigrid-docker:latest
-# commit a new image
-# sudo docker commit [CONTAINER_ID] [new_image_name]
-# sudo docker commit 852f2b256e44 unigrid/unigrid:edits
-# mount the volume to the image
-# docker run -it --name=blah_blah_blah --mount source=unigrid_data,destination=/root/.unigrid unigrid/unigrid:beta
-# docker run -it --name=server-test --mount source=data-volume,destination=/root/.unigrid unigrid/unigrid:beta
+# docker build --no-cache . --tag=unigrid/unigrid:latest
 FROM ubuntu:latest as builder
 LABEL org.unigrid.image.authors="UGD Software AB"
 LABEL version="0.0.1"
@@ -28,12 +22,12 @@ RUN apt-get install -y \
     cron \
     htop \
     openjdk-17-jre-headless
-ADD https://raw.githubusercontent.com/unigrid-project/unigrid-installer/main/service.sh /usr/local/bin/ugd_service
+COPY scripts/service.sh /usr/local/bin/ugd_service
 RUN chmod +x /usr/local/bin/ugd_service
-ADD https://raw.githubusercontent.com/unigrid-project/unigrid-installer/main/unigrid.sh /usr/local/bin/
+COPY scripts/unigrid.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/unigrid.sh
 RUN unigrid.sh root
-COPY entrypoint.sh /entrypoint.sh
+COPY sripts/entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["cron","-f", "-l", "2"]
---
+++ scripts/service.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+# Copyright © 2021-2022 The Unigrid Foundation, UGD Software AB
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the
+# addended GNU Affero General Public License as published by the Free Software Foundation, version 3
+# of the License (see COPYING and COPYING.addendum).
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+
+# You should have received an addended copy of the GNU Affero General Public License with this program.
+
+set -e
+. /lib/lsb/init-functions
+
+# Must be a valid filename
+NAME=unigrid
+PIDFILE=/run/$NAME.pid
+
+# Full path to executable
+DAEMON="/usr/bin/java -- -jar /usr/local/bin/groundhog.jar"
+
+# Options
+DAEMON_OPTS="start -t=false -l=/usr/local/bin/"
+
+# User to run the command as
+USER=root
+
+CLI='/usr/local/bin/unigrid-cli'
+
+export PATH="${PATH:+$PATH:}/usr/sbin:/sbin"
+
+CHECK_IF_RUNNING() {
+      GROUNDHOG="$(! pgrep -f groundhog &> /dev/null ; echo $?)"
+      echo "groundhog: ${GROUNDHOG}"
+      if [ "${GROUNDHOG}" = "0" ]; then
+      start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PIDFILE --chuid $USER --exec $DAEMON $DAEMON_OPTS
+      else
+      echo -e "Groundhog is running"
+      fi
+}
+
+case "$1" in
+  start)
+        echo -n "Starting daemon: "$NAME
+        start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PIDFILE --chuid $USER --exec $DAEMON $DAEMON_OPTS
+        echo "."
+        ;;
+  stop)
+        echo -n "Stopping daemon: "$NAME
+        start-stop-daemon --stop --quiet --oknodo --pidfile $PIDFILE
+        echo "."
+        ;;
+  restart)
+        echo -n "Restarting daemon: "$NAME
+        start-stop-daemon --stop --quiet --oknodo --retry 30 --pidfile $PIDFILE
+        sleep 0.3
+        start-stop-daemon --start --quiet -b -m --pidfile $PIDFILE --chuid $USER --exec $DAEMON $DAEMON_OPTS
+        echo "."
+        ;;
+  unigrid)
+        echo -e "`($CLI $2 $3 $4 $5)`"
+        ;;
+  check)
+        CHECK_IF_RUNNING
+        ;;
+  status)
+        status_of_proc -p $PIDFILE $DAEMON $NAME && exit 0 || exit $?
+        ;;
+
+  *)
+        echo "Usage: "$1" {start|stop|restart|unigrid|check <COMMAND>}"
+        exit 1
+esac
+
+
